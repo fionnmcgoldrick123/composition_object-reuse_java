f41cf532311c09f9452ba9799a52abe9ca2494ac
fionn mcgoldrick <fionnmc123@gmail.com>
2025-09-28 14:56:28 +0100

    extract AbstractSymmetricCypher to encapsulate shared symmetric key logic

--- src/ie/atu/sw/AESCypher.java
+++ src/ie/atu/sw/AESCypher.java
@@ -1,27 +1,15 @@
 package ie.atu.sw;
 
-import java.security.Key;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 
-public class AESCypher extends AbstractCypher {
-	private Key key;
-
+public class AESCypher extends AbstractSymmetricCypher {
 	public AESCypher() throws Throwable{
+		super();
 		KeyGenerator keyGen = KeyGenerator.getInstance("AES"); 
 		keyGen.init(128);
-		this.key = keyGen.generateKey();
+		this.setKey(keyGen.generateKey());
 		super.setCypher(Cipher.getInstance("AES/ECB/PKCS5Padding"));
-	}
-
-	@Override
-	public byte[] encrypt(byte[] plainText) throws Throwable {
-		return super.encrypt(plainText, key);
-	}
-
-	@Override
-	public byte[] decrypt(byte[] cypherText) throws Throwable {
-		return super.decrypt(cypherText, key);
 	}
 
 }
