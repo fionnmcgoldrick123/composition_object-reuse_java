0025f3679e575b6c5cb7a0a79a824b8401c9c055
fionn mcgoldrick <fionnmc123@gmail.com>
2025-09-28 14:19:59 +0100

    make AbstractCypher abstract with reusable key-based methods, update RSACypher to extend and delegate

--- src/ie/atu/sw/AbstractCypher.java
+++ src/ie/atu/sw/AbstractCypher.java
@@ -4,28 +4,39 @@ import java.security.Key;
 
 import javax.crypto.Cipher;
 
-public class AbstractCypher {
+public abstract class AbstractCypher {
 
-	protected Cipher cypher;
+	private Cipher cypher;
 
 	public AbstractCypher() {
 		super();
 	}
+	
+	public abstract byte[] encrypt(byte[] plainText) throws Throwable;
+	public abstract byte[] decrypt(byte[] cypherText) throws Throwable;
 
 	public byte[] encrypt(byte[] plainText, Key key) throws Throwable {
-		cypher.init(Cipher.ENCRYPT_MODE, key);
-		return cypher.doFinal(plainText);
+		getCypher().init(Cipher.ENCRYPT_MODE, key);
+		return getCypher().doFinal(plainText);
 	}
 
 	public byte[] decrypt(byte[] cypherText, Key key) throws Throwable {
-		cypher.init(Cipher.DECRYPT_MODE, key);
-		return cypher.doFinal(cypherText);
+		getCypher().init(Cipher.DECRYPT_MODE, key);
+		return getCypher().doFinal(cypherText);
 	}
 
 	@SuppressWarnings("removal")
 	@Override
 	protected void finalize() throws Throwable {
 		super.finalize();
+	}
+
+	protected Cipher getCypher() {
+		return cypher;
+	}
+
+	protected void setCypher(Cipher cypher) {
+		this.cypher = cypher;
 	}
 
 }
--- src/ie/atu/sw/RSACypher.java
+++ src/ie/atu/sw/RSACypher.java
@@ -5,13 +5,23 @@ import java.security.KeyPairGenerator;
 import javax.crypto.Cipher;
 
 public class RSACypher extends AbstractCypher {
-	KeyPair keyRing;
+	private KeyPair keyRing;
 
 	public RSACypher() throws Throwable {
 		super();
 		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
 		keyGen.initialize(2048);
 		keyRing = keyGen.generateKeyPair();
-		cypher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		super.setCypher(Cipher.getInstance("RSA/ECB/PKCS1Padding"));
+	}
+
+	@Override
+	public byte[] encrypt(byte[] plainText) throws Throwable {
+		return super.encrypt(plainText, keyRing.getPublic());
+	}
+
+	@Override
+	public byte[] decrypt(byte[] cypherText) throws Throwable {
+		return super.decrypt(cypherText, keyRing.getPrivate());
 	}
 }
