e7f5374ec4c42a8ef6af477e1fc1b07673f6a5ce
fionn mcgoldrick <fionnmc123@gmail.com>
2025-09-28 15:05:05 +0100

    replace AESCypher with generic SymmetricCypher supporting configurable algorithms

--- src/ie/atu/sw/Runner.java
+++ src/ie/atu/sw/Runner.java
@@ -2,7 +2,7 @@ package ie.atu.sw;
 
 public class Runner {
 	public static void main(String[] args) throws Throwable {
-		AbstractCypher cypher = new RSACypher();
+		AbstractCypher cypher = new SymmetricCypher("AES", 128, "AES/ECB/PKCS5Padding");
 		
 		byte[] s = cypher.encrypt(new String("Attack the castle wall at dawn").getBytes("UTF-8"));
 		
